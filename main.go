e82441915179c97dd29639dbfcd06d04af5cfd77
santos95mat <santos95.mat@gmail.com>
2023-09-22 19:29:35 -0300

    fix: clear de main package

--- main.go
+++ main.go
@@ -1,14 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/santos95mat/go-utils/CPF"
-)
-
-func main() {
-	var cpf string = "054.040.061-05"
-	isValid, err := CPF.IsValid(cpf)
-
-	fmt.Println(cpf, isValid, err)
-}
+func main() {}
